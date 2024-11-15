4997b503443acba3c36f0856134db62a662bc116
JACdeBruijn_X1 <181357207+JACdeBruijn@users.noreply.github.com>
2024-11-15 09:52:08 +0100

    Working function!!

--- Function for heatmap plots.R
+++ Function for heatmap plots.R
@@ -41,7 +41,7 @@ overview.tab <- overview.tab %>%
 # Loading in the correct WBAT data sets
 ################################################################################
 
-for(stationSet in unique(WBAT.tab$dataSet_station)){                              # Loop that will do everything for each unique dataSet_station that does also occur in overview.tab
+for(stationSet in unique(WBAT.tab$surveName)){                              # Loop that will do everything for each unique dataSet_station that does also occur in overview.tab
   print(stationSet)
   tab.filt <- WBAT.tab[WBAT.tab$dataSet_station == stationSet,]
   
@@ -70,3 +70,120 @@ for(stationSet in unique(WBAT.tab$dataSet_station)){                            
   }else{
     df.join.all <- rbind(df.join.all,df.join)
   }
+
+
+
+
+
+
+
+
+ggplot(subset(data, 
+              datetime > as.POSIXct("2021-07-13 10:05:29", tz = "UTC") & 
+                datetime < as.POSIXct("2021-07-13 18:05:29", tz = "UTC")), 
+       aes(x = interval_number, y = depth, fill = log10_SA)) +
+  geom_tile() +
+  geom_line(subset(data_depth_00,
+                   datetime > as.POSIXct("2021-07-13 10:05:29", tz = "UTC") & 
+                     datetime < as.POSIXct("2021-07-13 18:05:29", tz = "UTC")),
+            mapping = aes(y = (log10_SA*5)), color = "purple", size = 1) +
+  scale_fill_gradientn(colors = c("lightblue", "lightblue", "red"),
+                       values = c(0, 0.6, 1),
+                       limits = c(-5, 5),
+                       name = "log10(SA)") +
+  labs(title = "SA Values by Depth and Time", x = "Time", y = "Water depth (m)") +
+  scale_y_continuous(sec.axis = sec_axis(~ . / 5, 
+                                         name = "Total log10(SA) Values (Purple line)")) +
+  theme_minimal() 
+
+plot_intervals_in_chunks <- function(data, gap_interval = 10, num_iterations = 3, frequency, treshold, data_drop) {
+  # Preprocess the data
+  data <- data %>%
+    filter(treshold %in% c(treshold)) %>%
+    arrange(datetime) %>%
+    mutate(
+      SA = SA + 0.00001,
+      log10_SA = log10(SA),
+      interval_number = dense_rank(datetime)
+    ) %>%
+    arrange(interval_number) %>%
+    mutate(interval_number_with_gap = ifelse((interval_number %% gap_interval) == 0, NA, interval_number))
+  
+  # Filter and calculate the quantile logic
+  data <- data %>%
+    group_by(interval_number) %>%
+    mutate(
+      quantile_25 = quantile(log10_SA[depth != 0], data_drop, na.rm = TRUE),
+      log10_SA = ifelse(log10_SA < quantile_25 & depth != 0, -5.0, log10_SA),
+      SA_sum_filtered = sum(ifelse(log10_SA >= quantile_25 & depth != 0, SA, 0), na.rm = TRUE)
+    ) %>%
+    ungroup() %>%
+    mutate(SA = ifelse(depth == 0, SA_sum_filtered, SA)) %>%
+    select(-SA_sum_filtered, -quantile_25)
+  
+  # Separate data for depth = 0
+  data_depth_00 <- data %>%
+    filter(depth == 0) %>%
+    select(datetime, depth, SA, log10_SA, interval_number) %>%
+    mutate(log10_SA = log10(SA))
+  
+  # Define chunk size and iterations
+  total_intervals <- max(data$interval_number, na.rm = TRUE)
+  chunk_size <- 240
+  num_iterations <- min(num_iterations, ceiling(total_intervals / chunk_size))
+  
+  for (i in 1:num_iterations) {
+    # Subset data for the current chunk
+    start_interval <- (i - 1) * chunk_size + 1
+    end_interval <- min(i * chunk_size, total_intervals)
+    data_chunk <- data %>%
+      filter(interval_number >= start_interval & interval_number <= end_interval)
+    
+    if (nrow(data_chunk) == 0) {
+      message("No data left to process, stopping the loop.")
+      break
+    }
+    
+    # Create the heatmap plot
+    plot <- ggplot(data_chunk, aes(x = interval_number_with_gap, y = depth, fill = log10_SA)) +
+      geom_tile() +
+      geom_line(aes(y = depthIntegration), color = "red", size = .5) +
+      geom_line(
+        data = data_depth_00 %>%
+          filter(interval_number >= start_interval & interval_number <= end_interval),
+        aes(x = interval_number, y = log10_SA), 
+        color = "purple", size = 1
+      ) +
+      scale_fill_gradientn(
+        colors = c("lightblue", "lightblue", "red"),
+        values = c(0, 0.6, 1),
+        limits = c(-5, 5),
+        name = "log10(SA)"
+      ) +
+      labs(
+        title = paste0("log10(SA) at ", frequency, "kHz for ", treshold, "dB with datadrop of ", data_drop, "% - ", start_interval, " to ", end_interval),
+        x = "Time in Interval Number",
+        y = "Water Depth (m)"
+      ) +
+      theme_minimal() +
+      theme(
+        axis.text.x = element_text(angle = 45, hjust = 1)
+      )
+    
+    # Save the plot
+    output_folder <- file.path(resultPath, "SA_Heatmaps")
+    if (!dir.exists(output_folder)) dir.create(output_folder)
+    filename <- paste0(output_folder, "/SA_Heatmap_", frequency, "kHz_", treshold, "dB_datadrop_", data_drop, "%_", start_interval, "_to_", end_interval, ".png")
+    ggsave(filename, plot = plot, width = 10, height = 6)
+  }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Improvment heatmaps.R
@@ -0,0 +1,231 @@
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(tidyverse, lubridate)
+
+rm(list=ls())
+
+sourceDir <- function(directory) {
+  files <- list.files(directory, pattern = "\\.R$", full.names = TRUE)
+  for (file in files) {
+    source(file)
+  }
+}
+
+sourceDir(file.path('.','Function'))
+figurePath    <- file.path('.','Figures')
+dataPath      <- file.path('.','Data')
+resultPath    <- file.path('.','Results')
+
+################################################################################
+# Loading in the overview data sets
+################################################################################
+WBAT.tab <- read.csv(file.path(dataPath,'survey_db.csv'))                 
+WBAT.tab <- WBAT.tab[,c(1:5,8,9)]
+WBAT.tab$dataSet_station <- paste0(WBAT.tab$dataSet,'_',WBAT.tab$station)
+WBAT.tab <- WBAT.tab %>% 
+  filter(!(dataSet %in% c("2022-cpower", "2022-HKZ", "2023-HKN")))
+
+overview.tab <- read.csv(file.path(dataPath,'data_overview.csv'))               # Creating overview.tab from data_overview.csv
+overview.tab$stationSet <- paste0(overview.tab$dataSet,'_',overview.tab$station)
+overview.tab$pairingName <- paste0(overview.tab$dataSet,'_',overview.tab$pairing)
+overview.tab <- overview.tab %>% 
+  select(-c('year','dataSet','station')) %>%
+  select(stationSet, everything()) %>% 
+  filter(pairing != "-1")
+
+
+############################################################################################
+# Beginning steps of script
+
+# This works to load all the data files 
+############################################################################################
+
+
+for(i in unique(WBAT.tab$surveName)){                              # Loop that will do everything for each unique dataSet_station that does also occur in overview.tab
+  tab.filt <- WBAT.tab[WBAT.tab$surveName == i,]
+  
+  for(idxDataSet in tab.filt$surveName){                                          # Reading all the data on station and data set for pelegc fish data
+    load(file.path(dataPath, paste0('WBAT_',idxDataSet,'.RData')))
+    
+  }
+}
+
+
+############################################################################################
+# Improving the plots based on input Jeroen
+############################################################################################
+# Loading data
+WBAT_2021_BE_P1_belwind_70khz <- WBAT.all
+
+# Working with the -60dB data
+WBAT_2021_BE_P1_belwind_70khz_60freq <- WBAT_2021_BE_P1_belwind_70khz %>% 
+  filter(treshold %in% c("-60")) %>% 
+  arrange(datetime) %>%
+  mutate(SA = SA + 0.00001,
+         log10_SA = log10(SA),
+         interval_number = dense_rank(datetime))
+
+#######################
+
+data <- WBAT_2021_BE_P1_belwind_70khz_60freq %>% 
+  filter(depth != 0) %>% 
+  group_by(interval_number) %>% 
+  mutate(quantile_25 = quantile(log10_SA, 0.25, na.rm = T),
+         log10_SA = ifelse(log10_SA < quantile_25, -5.0, log10_SA)) %>%
+  ungroup() %>% 
+  
+  summarise(SA_sum_filtered  = sum(SA, na.rm = T)) %>%
+  right_join(WBAT_2021_BE_P1_belwind_70khz_60freq, by = "interval_number") %>%
+  mutate(SA = ifelse(depth == 0.0, SA_sum_filtered , SA),
+         log10_SA = ifelse(depth != 0 & log10_SA < quantile_25, -5.0, log10_SA)) %>% 
+  select(-SA_sum_filtered, -quantile_25)
+
+
+
+
+
+data <- WBAT_2021_BE_P1_belwind_70khz_60freq %>%
+  group_by(interval_number) %>%
+  mutate(quantile_25 = quantile(log10_SA[depth != 0], 0.0, na.rm = TRUE),    # Calculate the 25th percentile of log10_SA, ignoring NA values
+         log10_SA = ifelse(log10_SA < quantile_25 & depth != 0, -5.0, log10_SA),    # Replace log10_SA values below the 25th percentile with -5.0
+         SA_sum_filtered = sum(ifelse(log10_SA >= quantile_25 & depth != 0, SA, 0), na.rm = TRUE)) %>%    # Calculate the sum of SA (after filtering out low log10_SA values) for each interval_number
+  ungroup() %>%
+  mutate(SA = ifelse(depth == 0, SA_sum_filtered, SA)) %>%     # Replace SA at depth == 0.0 with SA_sum_filtered
+  select(-SA_sum_filtered, -quantile_25) 
+
+data_depth_00 <- data %>% 
+  filter(depth == 0) %>% 
+  select(datetime, depth, SA, log10_SA, interval_number) %>% 
+  mutate(log10_SA = log10(SA))
+  
+
+
+
+ggplot(data, aes(x = log10_SA)) +
+  geom_histogram(binwidth = .1, fill = "skyblue", color = "black") +
+  labs(title = "Distribution of SA Values at -60dB for 70kHz",
+       x = "log10(SA)",
+       y = "Frequency") +
+  theme_minimal()
+
+
+#######################
+
+ggplot(subset(data, 
+              datetime > as.POSIXct("2021-07-13 10:05:29", tz = "UTC") & 
+                datetime < as.POSIXct("2021-07-13 18:05:29", tz = "UTC")), 
+       aes(x = interval_number, y = depth, fill = log10_SA)) +
+  geom_tile() +
+  geom_line(subset(data_depth_00,
+                   datetime > as.POSIXct("2021-07-13 10:05:29", tz = "UTC") & 
+                     datetime < as.POSIXct("2021-07-13 18:05:29", tz = "UTC")),
+            mapping = aes(y = (log10_SA*5)), color = "purple", size = 1) +
+  scale_fill_gradientn(colors = c("lightblue", "lightblue", "red"),
+                       values = c(0, 0.6, 1),
+                       limits = c(-5, 5),
+                       name = "log10(SA)") +
+  labs(title = "SA Values by Depth and Time", x = "Time", y = "Water depth (m)") +
+  scale_y_continuous(sec.axis = sec_axis(~ . / 5, 
+                                         name = "Total log10(SA) Values (Purple line)")) +
+  theme_minimal() 
+  # theme(panel.grid = element_blank())
+
+
+################################################################################
+# Script for the new plots
+################################################################################
+
+# Define the function to plot the heatmap for a limited number of intervals (e.g., 3 iterations)
+plot_intervals_in_chunks <- function(data, gap_interval = 10, num_iterations = 3, frequency, treshold, data_drop) {
+  
+  data <- data %>% 
+    filter(treshold %in% c(treshold)) %>% 
+    arrange(datetime) %>%
+    mutate(SA = SA + 0.00001,
+           log10_SA = log10(SA),
+           interval_number = dense_rank(datetime)) %>% 
+    arrange(interval_number) %>% 
+    mutate(interval_number_with_gap = ifelse((interval_number %% gap_interval) == 0, NA, interval_number))
+  
+  # Ensure that data is ordered by interval_number (or datetime if needed)
+  
+  data <- data %>%
+    group_by(interval_number) %>%
+    mutate(quantile_25 = quantile(log10_SA[depth != 0], data_drop, na.rm = TRUE),    # Calculate the 25th percentile of log10_SA, ignoring NA values
+           log10_SA = ifelse(log10_SA < quantile_25 & depth != 0, -5.0, log10_SA),    # Replace log10_SA values below the 25th percentile with -5.0
+           SA_sum_filtered = sum(ifelse(log10_SA >= quantile_25 & depth != 0, SA, 0), na.rm = TRUE)) %>%    # Calculate the sum of SA (after filtering out low log10_SA values) for each interval_number
+    ungroup() %>%
+    mutate(SA = ifelse(depth == 0, SA_sum_filtered, SA)) %>%     # Replace SA at depth == 0.0 with SA_sum_filtered
+    select(-SA_sum_filtered, -quantile_25) 
+  
+  data_depth_00 <- data %>%
+    filter(depth == 0) %>%
+    select(datetime, depth, SA, log10_SA, interval_number) %>%
+    mutate(log10_SA = log10(SA))
+  
+  # Get the total number of interval_numbers
+  total_intervals <- max(data$interval_number, na.rm = TRUE)
+  chunk_size <- 240
+  num_iterations <- min(num_iterations, ceiling(total_intervals / chunk_size))
+  
+  for (i in 1:num_iterations) {
+    # Calculate the start and end interval numbers for the current chunk (240 intervals)
+    start_interval <- (i - 1) * chunk_size + 1
+    end_interval <- min(i * chunk_size, total_intervals)
+    data_chunk <- data %>% 
+      filter(interval_number >= start_interval & interval_number <= end_interval)
+    
+    if (nrow(data_chunk) == 0) {                                                
+      message("No data left to process, stopping the loop.")
+      break
+    }
+    
+    # Create the heatmap plot for the current chunk
+    plot <- ggplot(data_chunk, aes(x = interval_number_with_gap, y = depth, fill = log10_SA)) +
+      geom_tile() +
+      geom_line(data = data_depth_00 %>%
+                  filter(interval_number >= start_interval & interval_number <= end_interval),
+                mapping = aes(x = interval_number, y = (log10_SA*5)), color = "purple", size = 1) +
+      scale_fill_gradientn(colors = c("lightblue", "lightblue", "red"),
+                           values = c(0, 0.6, 1),
+                           limits = c(-5, 5),
+                           name = "log10(SA)") +
+      labs(
+        title = paste0("log10(SA) at ", frequency, "kHz for ", treshold, "dB with datadrop of ", data_drop, "% - ", start_interval, " to ", end_interval),
+        x = "Time in interval Number",
+        y = "Water depth (m)") +
+      scale_y_continuous(sec.axis = sec_axis(~ . / 5, 
+                                             name = "Total log10(SA) Values (Purple line)")) +
+      theme_minimal() +
+      theme(
+        # panel.grid = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1)  # Optional: tilt x-axis labels
+      )
+    
+    # Save the plot as a PNG file with the correct date and interval range in the filename
+    output_folder <- file.path(resultPath, "SA_Heatmaps")
+    if(!dir.exists(output_folder)) dir.create(output_folder)
+    filename <- paste0(output_folder, "/SA_Heatmap_", frequency, "kHz_", treshold, "dB_datadrop_", data_drop, "%_", start_interval, "_to_", end_interval, ".png")
+    ggsave(filename, plot = plot, width = 10, height = 6)
+  }
+}
+
+# Example usage with WBAT.2021_BE_belwind_70kHz_test_tres50 dataset, limiting to 3 iterations
+plot_intervals_in_chunks(WBAT_2021_BE_P1_belwind_70khz, gap_interval = 10, num_iterations = 3, frequency = 70, treshold = -60, data_drop = .75)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
